e4039a1606d1a0d536cc23821880eb092ba4e842
Manichanh <manichanh2002@gmail.com>
2023-09-20 14:57:17 +0700

    edit Navbar

--- src/layouts/NavbarLayout.vue
+++ src/layouts/NavbarLayout.vue
@@ -1,30 +1,84 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
 
 <template>
-  <div class="h-16 flex justify-between items-center px-5 bottom-1 relative lg:static">
+  <div class="h-16 flex justify-between items-center px-5 shadow-sm relative lg:static">
     <div class="flex justify-between items-center">
-      <a href="" class="mr-5"><i class="ri-menu-line text-4xl lg:hidden"></i></a>
+      <a href="" class="mr-5"><i class="ri-menu-line text-3xl lg:hidden"></i></a>
       <label class="input-group">
         <span><i class="ri-search-line text-xl"></i></span>
         <input type="text" placeholder="Search" class="input w-[10rem] sm:w-[20rem] px-4" />
       </label>
     </div>
-    <a href=""><i class="ri-more-2-fill text-3xl lg:hidden"></i></a>
-    <ul class="hidden lg:flex lg:gap-6">
-      <li>
-        <a href=""><i class="ri-inbox-2-line text-3xl"></i></a>
-      </li>
-      <li>
-        <a href=""><i class="ri-notification-2-line text-3xl"></i></a>
-      </li>
-      <li>
-        <a href=""
-          ><img
-            src="https://th.bing.com/th/id/R.0d3542651c776e42213c5fec41135d90?rik=Yb6lb52WvDq9sw&riu=http%3a%2f%2fimages2.fanpop.com%2fimages%2fphotos%2f5900000%2fKittens-cats-5979853-2560-1817.jpg&ehk=M5ssACU9nmKUthjfl6TlIir4JVJPNnlk7yo6Nx%2bUzus%3d&risl=&pid=ImgRaw&r=0"
-            alt=""
-            class="rounded-full w-8 h-8"
-        /></a>
-      </li>
-    </ul>
+    <div class="flex items-center justify-center">
+      <div>
+        <ul class="hidden lg:flex lg:items-center absolute lg:static gap-x-6">
+          <li>
+            <a href="">
+              <i class="ri-inbox-2-line text-3xl flex items-center justify-center">
+                <span class="font-sans block lg:hidden text-base mx-2 lg:mx-0">Inbox</span>
+              </i>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <i class="ri-notification-2-line text-3xl flex items-center justify-center">
+                <span class="font-sans block lg:hidden text-base mx-2 lg:mx-0">Notifications</span>
+              </i>
+            </a>
+          </li>
+          <li>
+            <a href=""
+              ><img
+                src="https://th.bing.com/th/id/R.5b1a7d81d5781ae876c40ef119b50bd6?rik=lAXGksIDSng7lw&pid=ImgRaw&r=0"
+                alt=""
+                class="rounded-full w-8 h-8"
+              />
+              <div class="block lg:hidden">
+                <div class="text-base font-medium">Phongsavanh Bank</div>
+                <span class="text-sm font-normal">Software Developer</span>
+              </div>
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="dropdown dropdown-end">
+        <label href="" tabindex="0">
+          <i class="ri-more-2-fill text-2xl lg:hidden"></i>
+        </label>
+        <ul
+          class="lg:flex lg:items-center lg:flex-row absolute lg:static dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-60"
+        >
+          <li>
+            <a href="">
+              <i class="ri-inbox-2-line text-2xl flex items-center justify-center">
+                <span class="font-sans block lg:hidden text-base mx-2 lg:mx-0">Inbox</span>
+              </i>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <i class="ri-notification-2-line text-2xl flex items-center justify-center">
+                <span class="font-sans block lg:hidden text-base mx-2 lg:mx-0">Notifications</span>
+              </i>
+            </a>
+          </li>
+          <li>
+            <a href=""
+              ><img
+                src="https://th.bing.com/th/id/R.5b1a7d81d5781ae876c40ef119b50bd6?rik=lAXGksIDSng7lw&pid=ImgRaw&r=0"
+                alt=""
+                class="rounded-full w-8 h-8"
+              />
+              <div class="block lg:hidden">
+                <div class="text-base font-medium">Phongsavanh Bank</div>
+                <span class="text-sm font-normal">Software Developer</span>
+              </div>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
